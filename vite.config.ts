da8b0df8e7ca2a54272970f4781a08f48e74e146
yukessh <130457471+yukessh@users.noreply.github.com>
2025-02-11 22:29:58 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  build: {
+    rollupOptions: {
+      input: './index.html', // Ensure correct input file
+    },
+  },
+  resolve: {
+    alias: {
+      '@': '/src', // Ensure correct alias for src
+    },
   },
 });
